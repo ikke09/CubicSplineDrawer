93695fb2af50f94257a6730e1933bf4bd2e797c8
Christian Bechtluft <development@fam-bechtluft.de>
2017-04-01 17:34:09 +0200

    Basic Structure
    + Canvas Events
    + Render Functions
    + Spline class added

--- SplineDrawer/SplineDrawer/MainWindow.xaml.cs
+++ SplineDrawer/SplineDrawer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RuntimeFunctionParser;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,119 @@ namespace SplineDrawer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Point> _inputPoints;
+        private Spline _spline;
         public MainWindow()
         {
             InitializeComponent();
+            _inputPoints = new List<Point>();
+            _spline = null;
         }
+
+        #region Render Functions
+
+        private void RenderSpline(Spline s)
+        {
+            if (s == null || s.PartialPolynomials.Count == 0)
+                return;
+
+            foreach (Function f in s.PartialPolynomials)
+            {
+                RenderFunction(f);
+            }
+        }
+
+        private void RenderPoints(List<Point> points)
+        {
+            foreach (Point p in points)
+            {
+                Ellipse e = new Ellipse();
+                e.Width = e.Height = 6;
+                e.Margin = new Thickness(p.X, p.Y, 0, 0);
+                e.Fill = Brushes.Black;
+                e.Stroke = Brushes.White;
+                e.StrokeThickness = 1;
+                mainCanvas.Children.Add(e);
+            }
+        }
+
+        private void RenderFunction(Function f)
+        {
+            if (f == null)
+                return;
+
+            double minX = double.MaxValue;
+            double maxX = double.MinValue;
+            foreach (Point p in _inputPoints)
+            {
+                if (p.X > maxX)
+                    maxX = p.X;
+
+                if (p.X < minX)
+                    minX = p.X;
+            }
+
+            double step = ((maxX - minX) / _inputPoints.Count) * 0.5;
+            Polyline pl = new Polyline();
+            pl.Stroke = Brushes.Red;
+            pl.StrokeThickness = 3;
+            PointCollection pc = new PointCollection();
+            for (double i = minX; i <= maxX; i += step)
+            {
+                Point p = new Point(i, f.Solve(i, 0));
+                pc.Add(p);
+            }
+            pl.Points = pc;
+            mainCanvas.Children.Add(pl);
+        }
+
+        #endregion
+
+        #region Helper Functions
+
+        private void Reset()
+        {
+            mainCanvas.Children.Clear();
+            _inputPoints.Clear();
+            _spline = null;
+        }
+
+        private void CalculateAndRenderSpline()
+        {
+            _spline = new Spline(_inputPoints);
+            RenderSpline(_spline);
+        }
+
+        #endregion
+
+        #region Canvas Events
+
+        private void mainCanvas_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Enter:
+                    CalculateAndRenderSpline();
+                    break;
+                case Key.R:
+                    Reset();
+                    break;
+            }
+        }
+
+        private void mainCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+            mainCanvas.Focusable = true;
+            mainCanvas.Focus();
+        }
+
+        private void mainCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point p = e.GetPosition(mainCanvas);
+            _inputPoints.Add(p);
+            RenderPoints(_inputPoints);
+        }
+
+        #endregion
     }
 }
---
+++ SplineDrawer/SplineDrawer/Spline.cs
@@ -0,0 +1,44 @@
+﻿using RuntimeFunctionParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SplineDrawer
+{
+    internal class Spline
+    {
+        private List<Point> _sourcePoints { get; set; }
+
+        private List<Function> _partialPolynomials;
+        public List<Function> PartialPolynomials
+        {
+            get { return _partialPolynomials; }
+            set { _partialPolynomials = value; }
+        }
+
+        public Spline()
+        {
+            _sourcePoints = new List<Point>();
+            PartialPolynomials = new List<Function>();
+        }
+
+        public Spline(List<Point> points)
+        {
+            _sourcePoints = points;
+            PartialPolynomials = CalculateSplineParts();
+        }
+
+        private List<Function> CalculateSplineParts()
+        {
+            if (_sourcePoints != null && _sourcePoints.Count < 3)
+            {
+                return null;
+            }
+
+            throw new NotImplementedException("Not Implemented");
+        }
+    }
+}
